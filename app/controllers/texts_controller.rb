f24e277bde5b56e499baaa339870a5c2e8bb9288
facingsouth <xin.d.liu@gmail.com>
2015-07-18 16:06:11 -0700

    add twilio

--- app/controllers/texts_controller.rb
+++ app/controllers/texts_controller.rb
@@ -1,6 +1,13 @@
 class TextsController < ApplicationController
 
   def send_text
+    text = params[:todo]
+    client = Twilio::REST::Client.new(ENV["TWILIO_ID"], ENV["TWILIO_TOKEN"])
+    client.messages.create(
+      from: '+16502730668',
+      to: '+16504902147',
+      body: text
+    )
     flash[:success] = "Text sent!"
     redirect_to todos_path
   end
