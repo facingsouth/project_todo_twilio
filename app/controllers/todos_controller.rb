5349ef9435ad5ce6e68690e064ba56a4b730b0cc
facingsouth <xin.d.liu@gmail.com>
2015-07-18 15:34:37 -0700

    todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -18,6 +18,27 @@ class TodosController < ApplicationController
       render :new
     end
   end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo.update(whitelist_params)
+      flash[:success] = 'You item has been updated.'
+      redirect_to todos_path 
+    else
+      flash[:error] = 'An error has occurred'
+      render :edit
+    end
+  end
+
+  def edit
+    @todo = Todo.find(params[:id])
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.destroy
+    redirect_to todos_path
+  end
   
   def whitelist_params
     params.require(:todo).permit(:item)
